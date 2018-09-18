1180e703c798e634a4ae2a2161f999c0f2b1e5f3
Akshath Jain <akshath.r.jain@gmail.com>
2018-09-18 10:45:41 -0400

    added skeleton

---
+++ EdgeDetection.js
@@ -0,0 +1,10 @@
+/*
+Name: Akshath Jain
+Date: 9/17/18
+Purpose: edge detection algorithm
+*/
+
+function detectEdges(){
+	var image = document.getElementById('image-container');
+	console.log(image); 
+}
